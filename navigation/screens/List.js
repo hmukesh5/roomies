f1379a83d4c498152bd3ac6cc26db655cc022cf0
Hemanth <hmukesh@outlook.com>
2023-01-28 18:50:20 -0600

    Update List.js

--- navigation/screens/List.js
+++ navigation/screens/List.js
@@ -3,12 +3,16 @@ import {View, Text} from 'react-native'
 
 export default function ListScreen({navigation}) {
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{fontSize: 26, fontWeight: 'bold'}}>
-                List Screen
-            </Text>
+        <View style={styles.container}>
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
